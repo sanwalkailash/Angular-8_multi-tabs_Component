8de6e5a88590e6531dca964dd4a0f2259773402f
sanwalkailash <sanwalkailash@gmail.com>
2020-03-21 12:28:00 +0530

    Dynamic tab generation demo

--- app/tab-group-dynamic-example.ts
+++ app/tab-group-dynamic-example.ts
@@ -1,31 +1,27 @@
-import {Component} from '@angular/core';
-import {FormControl} from '@angular/forms';
+import { Component } from "@angular/core";
+import { FormControl } from "@angular/forms";
 
 /**
  * @title Tab group with dynamically changing tabs
  */
 @Component({
-  selector: 'tab-group-dynamic-example',
-  templateUrl: 'tab-group-dynamic-example.html',
-  styleUrls: ['tab-group-dynamic-example.css'],
+  selector: "tab-group-dynamic-example",
+  templateUrl: "tab-group-dynamic-example.html",
+  styleUrls: ["tab-group-dynamic-example.css"]
 })
 export class TabGroupDynamicExample {
-  tabs = ['First', 'Second', 'Third'];
+  tabs = ["Search", "venue details 1", "venue details 2"];
   selected = new FormControl(0);
 
   addTab(selectAfterAdding: boolean) {
-    this.tabs.push('New');
-
-    if (selectAfterAdding) {
-      this.selected.setValue(this.tabs.length - 1);
-    }
+    this.tabs.push("New");
+    this.selected.setValue(this.tabs.length);
   }
 
   removeTab(index: number) {
     this.tabs.splice(index, 1);
   }
 }
-
 
 /**  Copyright 2018 Google Inc. All Rights Reserved.
     Use of this source code is governed by an MIT-style license that
